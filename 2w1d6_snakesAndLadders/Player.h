ca56cb93bbc2a1532b0ed04355cffc9dda51deb3
hkimca <harrison.kim.ca@gmail.com>
2017-07-01 12:13:43 -0400

    Initial commit

---
+++ 2w1d6_snakesAndLadders/Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  2w1d6_snakesAndLadders
+//
+//  Created by Seantastic31 on 01/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Player : NSObject
+
+@end
---
+++ 2w1d6_snakesAndLadders/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  2w1d6_snakesAndLadders
+//
+//  Created by Seantastic31 on 01/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
