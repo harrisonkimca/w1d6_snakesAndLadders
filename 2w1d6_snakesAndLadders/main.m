2ba017dc16cb157c6422f0c14160ebd78d06eb41
hkimca <harrison.kim.ca@gmail.com>
2017-07-01 12:12:14 -0400

    Initial Commit

---
+++ 2w1d6_snakesAndLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  2w1d6_snakesAndLadders
+//
+//  Created by Seantastic31 on 01/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
